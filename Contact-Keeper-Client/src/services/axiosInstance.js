668665462d59b5fa7c49ce746def30c5616ee980
Hoangvu <hoangvu1872k2@gmail.com>
2024-01-25 15:33:47 +0700

    push

--- Contact-Keeper-Client/src/services/axiosInstance.js
+++ Contact-Keeper-Client/src/services/axiosInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 // require("dotenv").config();
 
 const axiosInstance = axios.create({
-  // baseURL: "https://hoangvux-be-contactkeeper.onrender.com/",
-  baseURL: "http://localhost:5000/",
+  baseURL: "https://hoangvux-be-contactkeeper.onrender.com",
+  // baseURL: "http://localhost:5000/",
 });
 
 export default axiosInstance;
